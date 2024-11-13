6a7934bfa020e58be84dd322bcfacfd73abda6f7
kharva <parvadesai404@gmail.com>
2024-11-13 16:55:39 +0530

    [Add] var & io Q2

---
+++ 1.Var-and-IO/Q2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int a, b, c;
+
+    std::cout << "Enter three numbers: ";
+    std::cin >> a >> b >> c;
+
+    std::cout << "You entered " << a << ", " << b << " and " << c << ".\n";
+
+    return 0;
+}
