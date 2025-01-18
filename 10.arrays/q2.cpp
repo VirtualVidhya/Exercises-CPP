8f397a6e7dc7c4de73b1255afc23c99b459318dc
kharva <parvadesai404@gmail.com>
2025-01-18 17:09:12 +0530

    [Update] array que ans

---
+++ 10.arrays/q2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main()
+{
+    int grocery_item_prices[] = {40, 50, 30, 60, 70};
+
+    std::cout << "Price of the 2nd item: " << grocery_item_prices[1] << '\n';
+
+    return 0;
+}
---
+++ 10.arrays/q3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main()
+{
+    std::string grocery_items[] = {"cheese", "potato", "butter", "bread", "tomato"};
+
+    std::cout << "The last item: " << grocery_items[4] << '\n';
+
+    return 0;
+}
